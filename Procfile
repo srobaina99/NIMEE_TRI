dd9fa8a4be272f31feff1091ab5b34095673ea65
srobaina99 <132522109+srobaina99@users.noreply.github.com>
2024-09-27 18:54:15 -0300

    created User model

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 app: cd app && yarn && yarn dev
-api: cd plataforma_tri && pipenv run python3 manage.py runserver 8080
+api: cd plataforma_tri && pipenv run python3 manage.py runserver 8081
--- plataforma_tri/api/models.py
+++ plataforma_tri/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    faculty = models.CharField(max_length=255)
+    email = models.CharField(unique=True,max_length=255)
+    password = models.CharField(max_length=255)
